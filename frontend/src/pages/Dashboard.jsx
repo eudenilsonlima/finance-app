1f6ce583ba096a58be40463f8170af91e0cf374d
Denilson Lima <seu-email@gmail.com>
2026-02-24 17:26:26 -0300

    Atualiza├º├úo autom├ítica

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { api } from "../services/api";
+import api from "../services/api";
 import Grafico from "../components/Grafico";
 import "./Dashboard.css"; // Vamos criar este arquivo abaixo
 
