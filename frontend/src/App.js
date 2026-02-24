3d82246da1427c3f0f1cb93dfe9c8f01bee4986f
Denilson Lima <seu-email@gmail.com>
2026-02-23 22:56:32 -0300

    Atualização manual

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Finance App</h1>
+      <p>Bem-vindo ao seu aplicativo de finanças pessoais!</p>
+    </div>
+  );
+}
+
+export default App;
